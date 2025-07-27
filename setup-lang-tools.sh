5b3ec7011740732654aec8ce885a7d0efc5ed6b6
l3k4n <alowoojalekan@gmail.com>
2025-07-26 19:55:45 -0500

    set nvm install path
    - update nvm version
    - add nvm sourcing to custom export file

--- setup-lang-tools.sh
+++ setup-lang-tools.sh
@@ -2,13 +2,19 @@ source utils.sh
 
 # nodejs
 if query "Do you want to install nodejs?"; then
+    local PROFILE="" # prevent the script from writing to bashrc
+    local XDG_CONFIG_HOME="$HOME/.config" # ensure .nvm files are placed correctly
+
     inform "setting up nodejs"
     note "installing nvm"
-    curl -sS -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    curl -sS -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     note "installing nodejs v20"
     source $NVM_DIR/nvm.sh
     nvm install 20
+
+    custom_export "export NVM_DIR=\"\$([ -z \"\${XDG_CONFIG_HOME-}\" ] && printf %s \"\${HOME}/.nvm\" || printf %s "\${XDG_CONFIG_HOME}/nvm")\"
+        [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\""
 fi
 
 # c++
