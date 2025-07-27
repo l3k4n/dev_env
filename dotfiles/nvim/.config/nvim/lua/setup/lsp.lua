0db097fee54137807556c1961e1bb2591368b7c5
l3k4n <alowoojalekan@gmail.com>
2025-07-26 21:51:27 -0500

    give clangd.switch_source_header a keymap

--- dotfiles/nvim/.config/nvim/lua/setup/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/setup/lsp.lua
@@ -8,8 +8,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
       vim.keymap.set('n', keys, func, { buffer = args.buf, desc = desc })
     end
 
+    local clangd_switch_source_header = function()
+      if vim.fn.exists(":LspClangdSwitchSourceHeader") then
+        vim.cmd("LspClangdSwitchSourceHeader")
+      end
+    end
+
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    nmap('<C-s>', clangd_switch_source_header, '(clangd) Switch between source and header file')
     nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
     nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
     nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
@@ -37,7 +44,6 @@ function M.setup_servers(servers)
     ensure_installed = vim.tbl_keys(servers),
     handlers = {
       function(server_name)
-        local server = require('lspconfig')[server_name]
         local server_config = servers[server_name] or {}
         local setup = {
           capabilities = capabilities,
